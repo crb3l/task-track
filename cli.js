38ee47a56ee96c3676f56de508ec78b6313e7c6e
unknown <iotheodor@gmail.com>
2024-11-08 16:03:19 +0200

    added list task, almost added gui, have to add remove function

--- cli.js
+++ cli.js
@@ -1,5 +1,12 @@
+const prompt = require("prompt-sync")();
 const t = [];
 const u = [];
+const d = [];
+const w1 = [];
+const w2 = [];
+var cc = 0;
+var cp = 0;
+const  data = Date().now;
 
 // function get_task(task, i){
 // 	console.log('This is task no. '+i'.');
@@ -27,27 +34,57 @@ function set_status(){
 
 function create_tasklist()
 {
-	var c = prompt('How many tasks do you want to input?')
-	tasks.length = c;
-	for (let i=0; i<c; i++)
+	cc = parseInt(prompt('How many tasks do you want to input?'));
+	console.log(cc);
+	t.length = cc;
+	cp+=cc;
+	for (let i=0; i<cc; i++)
 	{	
 		t[i] = prompt('What task do you have?');
+		d[i] = prompt('What is the description of the task?');
 		u[i] = 1;
+		w1[i] = data.toUTCString();
+	}
+}
+
+function list_tasklist()
+{
+	console.log(cc);
+	t.length = cc;
+	for (let i=0; i<cc; i++)
+	{	
+		console.log(t[i]);
+	}
+}
+
+function check_task()
+{
+	for (let i=0;i<cc;i++)
+	{
+		if(u[i] == 1)
+			console.log("Task"+i+"is in progress and was created on"+w1);
+		else if (u[i] == 2)
+			console.log("Task"+i+" is done. Date of completion: "+w2);
 	}
 }
 
 function update_tasklist()
 {
-	let i = prompt('Which task did you ocmpplete?');
+	let i = prompt('Which task did you complete?\n');
 	u[i] += 1;
+	w2[i] = w2[i] = data.toUTCString();
 }
 
 create_tasklist();
-// function list_tasklist()
-// {
-	
-// 	for (let i=0; i<c; i++)
-// 	{	
-		
-// 	}
-// }
+list_tasklist();
+
+
+
+function run()
+{
+	var y=9;
+	while (y)
+	{
+		y= prompt('Gday! Whatcha wanna do?\n\n 1. Create tasks. \n ');
+	}
+}
